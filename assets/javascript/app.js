c4ebb42f2009f75fadd6041c20f1b0b443dd9ffe
Brian Rizo <codingbr@gmail.com>
2018-01-23 19:25:05 -0500

    added https for api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -50,7 +50,7 @@ function reset(){
         
         var animal = $(this).attr("data-name");
         console.log(animal);
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         animal + "&api_key=dc6zaTOxFJmzC&limit=10";
         console.log(queryURL);
         // Creating an AJAX call for the specific animal button being clicked
